4d84e189d5da54911bcd4b342d7ed90b0f069026
Audumbar Kalokhe <audumbar.mescorp@gmail.com>
2025-02-27 11:00:15 +0530

    commite by shubham

--- src/main/java/com/add/Jenkinslaunch1Application.java
+++ src/main/java/com/add/Jenkinslaunch1Application.java
@@ -9,6 +9,7 @@ public class Jenkinslaunch1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Jenkinslaunch1Application.class, args);
 		System.out.println("Hi");
+		System.out.println("Hi2");
 	}
 
 }
