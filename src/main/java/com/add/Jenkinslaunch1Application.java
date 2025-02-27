1dd22e2dd8b115853c0decef3028ace98f48d933
Audumbar Kalokhe <audumbar.mescorp@gmail.com>
2025-02-27 09:51:51 +0530

    commit by shubham

---
+++ src/main/java/com/add/Jenkinslaunch1Application.java
@@ -0,0 +1,14 @@
+package com.add;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkinslaunch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkinslaunch1Application.class, args);
+		System.out.println("Hi");
+	}
+
+}
